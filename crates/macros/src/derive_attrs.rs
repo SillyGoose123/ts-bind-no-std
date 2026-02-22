5438dc981c0da885fe3109cae3cc4a51643e5904
Anian Barthel <mail@anian-barthel.de>
2026-02-22 18:18:02 +0100

    Add type export derive attribute

--- crates/macros/src/derive_attrs.rs
+++ crates/macros/src/derive_attrs.rs
@@ -1,84 +1,103 @@
 use syn::Attribute;
 
+use crate::parsers::field_attributes::get_nested_value;
 use crate::rename_all::RenameAll;
 use std::path::PathBuf;
-use crate::parsers::field_attributes::get_nested_value;
 
 #[derive(Debug)]
 pub struct DeriveAttrs {
-  name: String,
-  rename_all: Option<RenameAll>,
-  export: Option<PathBuf>,
+    name: String,
+    rename_all: Option<RenameAll>,
+    export: Option<PathBuf>,
+    pub enum_type_export: Option<bool>,
 }
 
 impl DeriveAttrs {
-  pub fn from(struct_name: String, attrs: &Vec<Attribute>) -> Self {
-    let mut struct_attrs = Self {
-      name: struct_name,
-      rename_all: None,
-      export: None,
-    };
+    pub fn from(struct_name: String, attrs: &Vec<Attribute>) -> Self {
+        let mut struct_attrs = Self {
+            name: struct_name,
+            rename_all: None,
+            export: None,
+            enum_type_export: None,
+        };
 
-    Self::parse_attrs(&mut struct_attrs, attrs);
+        Self::parse_attrs(&mut struct_attrs, attrs);
 
-    struct_attrs
-  }
+        struct_attrs
+    }
 
-  fn parse_attrs(struct_attrs: &mut Self, attrs: &Vec<Attribute>) {
-    attrs.iter().for_each(|attr| {
-      if attr.path().is_ident("ts_bind") {
-        attr.parse_nested_meta(|meta| {
-          let path = &meta.path;
+    fn parse_attrs(struct_attrs: &mut Self, attrs: &Vec<Attribute>) {
+        attrs.iter().for_each(|attr| {
+            if attr.path().is_ident("ts_bind") {
+                attr.parse_nested_meta(|meta| {
+                    let path = &meta.path;
 
-          let ident = path.get_ident();
+                    let ident = path.get_ident();
 
-          if let Some(ident) = ident {
-            let ident_str = ident.to_string();
+                    if let Some(ident) = ident {
+                        let ident_str = ident.to_string();
 
-            match ident_str.as_str() {
-              "rename" => {
-                let value = get_nested_value(&meta)
-                  .expect("Failed to parse rename attribute");
+                        match ident_str.as_str() {
+                            "rename" => {
+                                let value = get_nested_value(&meta)
+                                    .expect("Failed to parse rename attribute");
 
-                struct_attrs.name = value;
-              }
-              "rename_all" => {
-                let value = get_nested_value(&meta)
-                  .expect("Failed to parse rename attribute");
+                                struct_attrs.name = value;
+                            }
+                            "rename_all" => {
+                                let value = get_nested_value(&meta)
+                                    .expect("Failed to parse rename attribute");
 
-                struct_attrs.rename_all = RenameAll::from_string(&value)
-              }
-              "export" => {
-                let value = get_nested_value(&meta)
-                  .expect("Failed to parse export attribute");
+                                struct_attrs.rename_all = RenameAll::from_string(&value)
+                            }
+                            "export" => {
+                                let value = get_nested_value(&meta)
+                                    .expect("Failed to parse export attribute");
 
-                struct_attrs.export = Some(PathBuf::from(value));
-              }
-              _ => {
-                panic!("Invalid attribute name: {}", ident_str);
-              }
+                                struct_attrs.export = Some(PathBuf::from(value));
+                            }
+                            "enum_type_export" => {
+                                let value = get_nested_value(&meta)
+                                    .expect("Failed to parse rename attribute");
+                                struct_attrs.enum_type_export = Some(
+                                    value
+                                        .parse()
+                                        .expect("Failed to parse enum_type_export attribute"),
+                                );
+                            }
+                            _ => {
+                                panic!("Invalid attribute name: {}", ident_str);
+                            }
+                        }
+                    }
+
+                    Ok(())
+                })
+                .expect("Failed to parse nested meta");
             }
-          }
+        });
+    }
 
-          Ok(())
-        })
-          .expect("Failed to parse nested meta");
-      }
-    });
-  }
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
 
-  pub fn get_name(&self) -> &String {
-    &self.name
-  }
+    pub fn get_export_path(&self, is_enum: bool) -> PathBuf {
+        self.export
+            .clone()
+            .unwrap_or_else(|| PathBuf::new().join("bindings"))
+            .join(format!(
+                "{}{}.ts",
+                self.get_name(),
+                if is_enum { "" } else { ".d" }
+            ))
+    }
 
-  pub fn get_export_path(&self, is_enum: bool) -> PathBuf {
-    self.export
-      .clone()
-      .unwrap_or_else(|| PathBuf::new().join("bindings"))
-      .join(format!("{}{}.ts", self.get_name(), if is_enum { "" } else { ".d" }))
-  }
+    pub fn get_rename_all(&self) -> Option<&RenameAll> {
+        self.rename_all.as_ref()
+    }
 
-  pub fn get_rename_all(&self) -> Option<&RenameAll> {
-    self.rename_all.as_ref()
-  }
+    pub fn get_enum_type_export(&self) -> Option<&bool> {
+        self.enum_type_export.as_ref()
+    }
 }
